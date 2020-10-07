c7260b7217833adbd94fe431e548d39835f8a907
Heyimlulu <lucas.rouillermonay@edueptm.ch>
2020-10-07 11:06:07 +0200

    Initial Commit

---
+++ src/build/precache-manifest.55651ca955f087c8487eb3f8b6b11a4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a52913d876fc485f4dac77a1dc221e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "977832d5cc33de8548ae",
+    "url": "/static/css/main.04ba2c40.chunk.css"
+  },
+  {
+    "revision": "4b55d06576f5836f2679",
+    "url": "/static/js/2.e80f20af.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e80f20af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "977832d5cc33de8548ae",
+    "url": "/static/js/main.be8e9fd6.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
